ddfa677b75b36582334ec51341623f87ab3928bf
HamitBerkCeyhan <109693570+HamitBerkCeyhan@users.noreply.github.com>
2022-08-12 23:40:04 +0300

    Add files via upload

---
+++ pd2lab4-5-1.soru.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int n,*ptr,eb=0;
+	printf("girilecek sayi adedi: ");
+	scanf("%d",&n);
+	
+	ptr=(int *)calloc(n,sizeof(int));
+	if(ptr==NULL)
+	{
+		printf("Yeterli Hafiza Yok");
+	} 
+	{  
+	    for(int i=0; i<n; ++i)
+	    {
+		printf("Dizi Elemanlarini giriniz:\n ",i+1);
+		scanf("%d",ptr+i);
+          if(*(ptr+i)>eb)
+	      {
+	        eb=*(ptr+i);
+	      }  
+		}	
+		printf("En Buyuk = %d",eb);
+    }
+	return 0;
+}
